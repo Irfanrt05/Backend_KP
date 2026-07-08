aeb8f430d81bcbb7f587d7f69f15624fa514b17e
Muhammad Irfan Hakim <irfanhakim.4320@gmail.com>
2026-07-08 21:58:24 +0700

    backend lagi

--- server.js
+++ server.js
@@ -4,8 +4,26 @@ dotenv.config();
 import app from "./app.js";
 import { sequelize } from "./models/index.js";
 import { seedDefaultAdmin } from "./utils/seedAdmin.js";
+import puppeteer from "puppeteer";
 
 const PORT = process.env.PORT || 5000;
+
+// --- FIX PUPPETEER UNTUK RAILWAY ---
+// Kita buat fungsi helper untuk meluncurkan browser
+export const launchBrowser = async () => {
+  return await puppeteer.launch({
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+    ],
+    headless: "new",
+  });
+};
+// -----------------------------------
 
 try {
   await sequelize.authenticate();
